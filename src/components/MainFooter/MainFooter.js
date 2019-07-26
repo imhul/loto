ef3556148533fc6370a855423686f791c21b0d03
Im-Hul <blashirk@gmail.com>
2019-07-27 00:04:47 +0300

    footer update

--- src/components/MainFooter/MainFooter.js
+++ src/components/MainFooter/MainFooter.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Row, Col, Icon, } from 'antd';
+
+// Components
+import BottomMenu from './BottomMenu';
 
 const { Footer } = Layout;
 
-const MainFooter = () => <Footer>Footer</Footer>;
+const MainFooter = () => (
+    <Footer>
+        <Row gutter={24} type="flex" justify="space-around">
+            <Col span={6}><BottomMenu /></Col>
+            <Col span={6}><BottomMenu /></Col>
+            <Col span={6}><BottomMenu /></Col>
+            <Col span={6}>
+                <a href="#github" title="github">
+                    <Icon type="github" />
+                </a>
+                <a href="#instagram" title="instagram">
+                    <Icon type="instagram" />
+                </a>
+                <a href="#facebook" title="facebook">
+                    <Icon type="facebook" />
+                </a>
+                <a href="#twitter" title="twitter">
+                    <Icon type="twitter" />
+                </a>
+            </Col>
+        </Row>
+    </Footer>
+);
 
 export default MainFooter;
